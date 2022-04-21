2eeb028fdea1b9c8eb018c69dddf5c504a11be96
takahiro-it <tk.ryu.07@gmail.com>
2022-04-21 16:05:50 +0900

    予算画面遷移まで実装

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,4 +1,21 @@
 class BudgetsController < ApplicationController
   def index
+    @budgets = Budget.all  
   end
+
+  def new
+
+  end
+
+  def create
+    Budget.create(food_expenses: params[:food_expenses])
+    Budget.create(dining_expenses: params[:dining_expenses])
+    Budget.create(daily_necessities: params[:daily_necessities])
+    redirect_to "/budgets"
+  end
+
+  private
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # get 'wallets/index'
   root to: "wallets#index"
   get "budgets" => "budgets#index"
+  get "budgets/new" => "budgets#new"
+  post "budgets/create" => "budgets#create"
+
+
+
   get "spendings" => "spendings#index"
 
   get "tasks" => "tasks#index"
